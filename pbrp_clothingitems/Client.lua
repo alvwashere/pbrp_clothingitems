39d911e480bda34983d907380fc61eb2e128380e
Alv <72032190+alvwashere@users.noreply.github.com>
2023-10-15 16:28:33 +0100

    Update Client.lua

--- pbrp_clothingitems/Client.lua
+++ pbrp_clothingitems/Client.lua
@@ -1,14 +1,21 @@
-local QBCore = exports['qb-core']:GetCoreObject() 
 updateClothing = function()
 	local playerPed = PlayerPedId()
-	QBCore.Functions.TriggerCallback('pbrpClothingItems:GetClothes', function(outfitname) for i = 1, #Config do if Config[i].itemName == outfitname then for k, v in pairs(Config[i].set) do SetPedComponentVariation(playerPed, v.c1, v.c2, v.c3, 0) end end end end)
+	ESX.TriggerServerCallback('pbrpClothingItems:GetClothes', function(outfitname) 
+		for i = 1, #Config do 
+			if Config[i].itemName == outfitname then 
+				for k, v in pairs(Config[i].set) do 
+					SetPedComponentVariation(playerPed, v.c1, v.c2, v.c3, 0) 
+				end 
+			end 
+		end 
+	end)
 end
 
 RegisterNetEvent('pbrpClothingItems')
 AddEventHandler('pbrpClothingItems', function(number, label)
 	local playerPed = PlayerPedId()
 	for k, v in pairs(Config[number].set) do SetPedComponentVariation(playerPed, v.c1, v.c2, v.c3, 0) end
-	QBCore.Functions.Notify("You've put on " .. label, 'success', 1500)
+	ESX.ShowNotification("You've put on " .. label, 'success', 1500)
 end)
 
 Citizen.CreateThread(function()
